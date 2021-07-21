a2145193f46c4e799bf734fd44b0b9822d073540
misindelka <misindelka.dev@gmail.com>
2021-07-21 09:48:04 +0200

    minor upd

--- src/api/images.tsx
+++ src/api/images.tsx
@@ -2,10 +2,10 @@
 // Backgrounds
 
 export const BackgroundVegTable =
-	'http://localhost:1337/uploads/vegTable_2bf1219e9b.jpeg';
+	'http://localhost:1337/uploads/large_vegTable_d571b8207e_5b5b962ef1.jpeg';
 
 export const BackgroundWooden =
-	'http://localhost:1337/uploads/wooden_2ca61e0ac3.jpeg';
+	'http://localhost:1337/uploads/medium_wooden_2ca61e0ac3_2870ab69f3.jpeg';
 
 export const UnderConstruction =
 	'http://localhost:1337/uploads/under-construction_a971a2c2e5.png';
@@ -13,4 +13,4 @@ export const UnderConstruction =
 // Logas
 
 export const LogoBlackText =
-	'http://localhost:1337/uploads/text-logo_cierne_90ceb7074e.png';
+	'http://localhost:1337/uploads/medium_text-logo_cierne_90ceb7074e_4db00c511c.png';
